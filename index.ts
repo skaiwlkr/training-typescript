98570f76995c57cd65462ea4eeeedcd00950d79c
Sebastian Kaier <22768625+skaiwlkr@users.noreply.github.com>
2025-05-12 07:50:26 +0200

    added boolean

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 let age: number = 31; // Type number
 let firstName: string = "John"; // Type string
-
+let isStudent: boolean = true; // Type boolean
 
 age = 33;
+firstName = "Sebastian";
+isStudent = false;
 
 console.log(age);
