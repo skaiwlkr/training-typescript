7df7ccab4e227b95731763294e229aa4d79c2cdd
Sebastian Kaier <22768625+skaiwlkr@users.noreply.github.com>
2025-05-12 07:54:04 +0200

    added comments to structure the code

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
+// 01 VARIABLES
+// 01-01 VARIABLES WITH MANUAL TYPE ASSIGNMENT
 let age: number = 31; // Type number
 let firstName: string = "John"; // Type string
 let isStudent: boolean = true; // Type boolean
 
+// Overwrite variables
 age = 33;
 firstName = "Sebastian";
 isStudent = false;
 
-console.log(age);
+// 01-02 VARIABLES WITH AUTOMATIC TYPE ASSIGNMENT
+let planet = "Mars";
+let moons = 2;
+let isLarge = false;
